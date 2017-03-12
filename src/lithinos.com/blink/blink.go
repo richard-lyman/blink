bf64143df346724b2e844d074f5aa3e6764e62de
Richard Lyman <richard.lyman@gmail.com>
2017-03-11 23:20:30 -0700

    .

--- src/lithinos.com/blink/blink.go
+++ src/lithinos.com/blink/blink.go
@@ -8,18 +8,19 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 3 {
+	if len(os.Args) < 3 {
 		fmt.Println(`
-You must provide exactly two arguments.
-The first is the folder to watch.
-The second is the command to run when a change is seen.
+You must provide at least two arguments.
+The first is the command to run when a change is seen.
+The second, *and all remaining arguments*, are the items that will be watched - whose changes trigger the given command.
 
-    E.g.: ./bin/blink /some/folder some_script.sh
-    
+    E.g.: ./bin/blink some_script.sh /some/folder some_file
+
 `)
 		os.Exit(1)
 	}
-	fmt.Printf("Will run '%s' when anything changes\n", os.Args[2])
+	script := os.Args[1]
+	fmt.Printf("Will run '%s' when anything changes\n", script)
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		fmt.Println(err)
@@ -27,18 +28,20 @@ The second is the command to run when a change is seen.
 	}
 	defer watcher.Close()
 
-	err = watcher.Add(os.Args[1])
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	for _, v := range os.Args[2:] {
+		err = watcher.Add(v)
+		if err != nil {
+			fmt.Println("Failed to watch given argument:", v, err)
+			os.Exit(1)
+		}
+		fmt.Println("Watching:", v)
 	}
-	fmt.Println("Watching:", os.Args[1])
 
 	for {
 		select {
 		case <-watcher.Events:
 			fmt.Print("*")
-			c := exec.Command(os.Args[2])
+			c := exec.Command(script)
 			err := c.Run()
 			if err != nil {
 				fmt.Printf("\n\nERROR:\n%s\n\n", err)
